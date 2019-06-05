10c08488ae7edb4bf127116d37eade7c37ac219f
daczarne <daniel.czarnievicz@gmail.com>
2019-06-05 20:27:37 -0300

    le data sobre monedas (niveles del factor id_moneda)

--- compras_estatales.R
+++ compras_estatales.R
@@ -7,7 +7,27 @@ library(lubridate)
 library(stringr)
 library(rvest)
 
-compras <- readr::read_csv("Csv/comprasEstatales.csv")
+#### Descarga y le la base de monedas ####
+url_monedas <- "https://www.comprasestatales.gub.uy/comprasenlinea/jboss/reporteMonedas.do"
+monedas <- read_lines(url_monedas, locale = locale(encoding = "Latin1"))[3] %>% 
+   str_replace(pattern = "^(<monedas>)", replacement = "") %>% 
+   str_replace(pattern = "(</monedas>)$", replacement = "") %>% 
+   str_replace_all(pattern = "/>", replacement = "/>\\\n") %>% 
+   str_split(pattern = "\\n") %>% 
+   .[[1]] %>% 
+   tibble(x = .) %>% 
+   mutate(x = str_replace(string = x, pattern = "<moneda ", replacement = ""),
+          x = str_replace_all(string = x, pattern = "(\")([[:space:]])([a-z])", replacement = "\\1@\\3")) %>% 
+   separate(x, sep = "@",
+            into = c("id_moneda", "desc_moneda", "sigla_moneda", "id_moneda_arbitraje")) %>% 
+   mutate(id_moneda = as.numeric(str_replace_all(string = id_moneda, pattern = "[^0-9]", replacement = "")),
+          desc_moneda = str_replace_all(string = desc_moneda, pattern = "(^desc-moneda=\")|(\")$", replacement = ""),
+          sigla_moneda = str_replace_all(string = sigla_moneda, pattern = "(^sigla-moneda=\")|(\")", replacement = ""),
+          id_moneda_arbitraje = as.numeric(str_replace_all(string = id_moneda_arbitraje, pattern = "[^0-9]", replacement = ""))) %>% 
+   filter(!is.na(id_moneda))
+
+
+compras <- readr::read_csv("Csv/comprasEstatalesrefactor.csv")
 # all.equal(compras$X1, seq(0, dim(compras)[1]-1, 1))
 # comentario: revisar parseo....hay cosas raras (por ejemplo `@id_compra` es character)
 
@@ -30,17 +50,6 @@ compras <- compras %>%
    mutate(id = `X1`,
           fecha_compra = lubridate::dmy(fecha_compra),
           fecha_pub_adj = lubridate::dmy_hm(fecha_pub_adj))
-
-url_monedas <- "https://www.comprasestatales.gub.uy/comprasenlinea/jboss/reporteMonedas.do"
-read_lines(url_monedas)[3] %>% 
-   str_replace(pattern = "^(<monedas>)", replacement = "") %>% 
-   str_replace(pattern = "(</monedas>)$", replacement = "") %>% 
-   str_replace_all(pattern = "/>", replacement = "/>\\\n") %>% 
-   str_split(pattern = "\\n") %>% 
-   .[[1]] %>% 
-   tibble(x = .) %>% 
-   mutate(x = str_replace(string = x, pattern = "<moneda ", replacement = "")) 
-
 
 # Variable classes
 tibble(variables = names(sapply(compras, class)),
