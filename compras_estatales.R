9d2633e3a52013a37edf15dcaa45d652e7d3ed1f
daczarne <daniel.czarnievicz@gmail.com>
2019-06-05 23:38:11 -0300

    define clases correctas para las variables

--- compras_estatales.R
+++ compras_estatales.R
@@ -21,41 +21,36 @@ monedas <- read_lines(url_monedas, locale = locale(encoding = "Latin1"))[3] %>%
    separate(x, sep = "@",
             into = c("id_moneda", "desc_moneda", "sigla_moneda", "id_moneda_arbitraje")) %>% 
    mutate(id_moneda = as.numeric(str_replace_all(string = id_moneda, pattern = "[^0-9]", replacement = "")),
-          desc_moneda = str_replace_all(string = desc_moneda, pattern = "(^desc-moneda=\")|(\")$", replacement = ""),
+          desc_moneda = str_to_title(str_replace_all(string = desc_moneda, pattern = "(^desc-moneda=\")|(\")$", replacement = "")),
           sigla_moneda = str_replace_all(string = sigla_moneda, pattern = "(^sigla-moneda=\")|(\")", replacement = ""),
           id_moneda_arbitraje = as.numeric(str_replace_all(string = id_moneda_arbitraje, pattern = "[^0-9]", replacement = ""))) %>% 
    filter(!is.na(id_moneda))
 
 
 compras <- readr::read_csv("Csv/comprasEstatalesrefactor.csv")
-# all.equal(compras$X1, seq(0, dim(compras)[1]-1, 1))
-# comentario: revisar parseo....hay cosas raras (por ejemplo `@id_compra` es character)
-
-# id_compra <- as.numeric(compras$`@id_compra`)
-# pudo_parsear <- NULL
-# for (i in 1:dim(compras)[1]) {
-#    if (is.na(id_compra)[i] == TRUE) {
-#       pudo_parsear[i] <- "No"
-#    }
-#    else {
-#       pudo_parsear[i] <- "Yes"
-#    }
-# }
-# table(pudo_parsear)
-
-
-#### Por ahora sigo como si todo estuviera bien ####
-names(compras) <- str_replace_all(string = names(compras), pattern = "[`|@]", replacement = "")
 compras <- compras %>% 
-   mutate(id = `X1`,
+   mutate(apel = fct_recode(apel, "No" = "N", "Yes" = "S"),
+          es_reiteracion = fct_recode(es_reiteracion, "No" = "N", "Yes" = "S"),
+          estado_compra = as.factor(estado_compra),
           fecha_compra = lubridate::dmy(fecha_compra),
-          fecha_pub_adj = lubridate::dmy_hm(fecha_pub_adj))
+          fecha_pub_adj = lubridate::dmy_hm(fecha_pub_adj),
+          fondos_rotatorios = fct_recode(fondos_rotatorios, "No" = "N", "Yes" = "S"),
+          id_inciso = as.factor(id_inciso),
+          id_moneda_monto_adj = factor(id_moneda_monto_adj, levels = monedas$desc_moneda),
+          id_tipo_resol = as.factor(id_tipo_resol),
+          id_tipocompra = as.factor(id_tipocompra),
+          id_ue = as.factor(id_ue),
+          nro_ampliacion = as.factor(nro_ampliacion),
+          subtipo_compra = as.factor(subtipo_compra))
 
 # Variable classes
 tibble(variables = names(sapply(compras, class)),
        classes = sapply(compras, class)) %>%
    unnest() %>% 
    count(classes)
+
+# adjudicaciones -> detalle de la compra (de la factura)
+# oferentes -> todos los que participaron 
 
 
 #################################
