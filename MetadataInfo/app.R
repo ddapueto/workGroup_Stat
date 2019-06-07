edfd3f148cdc562b952fc49bef4b57159b002d35
ddapueto <ddapueto@despegar.com>
2019-06-06 23:25:44 -0300

    app with metadata

---
+++ MetadataInfo/app.R
@@ -0,0 +1,60 @@
+
+library(shiny)
+library(shinythemes)
+library(here)
+library(tidyverse)
+
+#generate data
+path <- here()
+path_oferantes <- paste(path,"/Csv/metadataOferantes.csv", sep="")
+path_adj <- paste(path,"/Csv/metadataAdjudicaciones.csv", sep="")
+path_comp <- paste(path,"/Csv/metadataCompras.csv", sep="")
+oferantes <- read_csv(path_oferantes)
+adjudicaciones <- read_csv(path_adj)
+compras <- read_csv(path_comp)
+
+#create Ui 
+ui <- tagList(
+  shinythemes::themeSelector(),
+  navbarPage(
+    "Trabajo Grupal App",
+    tabPanel("Navbar 1",
+             sidebarPanel(
+               radioButtons("metadata", "Metadata segun datasets: ",
+                                  c("Compras Estatales" = "compras" ,
+                                    "Adjudicaciones" = "adj",
+                                    "Oferentes" = "ofe"),selected = 'compras')
+             ),
+             mainPanel(
+               tabsetPanel(
+                 tabPanel("Metadata Info",
+                          h4("Table"),
+                          tableOutput("table")
+                          
+                 ),
+                 tabPanel("Codigueras", "This panel is intentionally left blank")
+               )
+             )
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$table <- renderTable({
+    if (input$metadata == "compras"){
+      df <-as.data.frame(compras)
+    }
+    else if (input$metadata == "adj"){
+      df <-as.data.frame(adjudicaciones)
+    }
+    else if (input$metadata == "ofe"){
+      df <- as.data.frame(oferantes)
+    }
+    df
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
