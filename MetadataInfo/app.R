8cbe137d4172738e039b5845df4237cd5ad9c845
daczarne <daniel.czarnievicz@gmail.com>
2019-06-18 22:09:49 -0300

    cambia nombres tabla codigueras

--- MetadataInfo/app.R
+++ MetadataInfo/app.R
@@ -6,10 +6,10 @@ library(tidyverse)
 
 # generate data
 path <- here::here()
-path_oferantes <- paste(path,"/Data/Csv/metadataOferantes.csv", sep="")
-path_adj <- paste(path,"/Data/Csv/metadataAdjudicaciones.csv", sep="")
-path_comp <- paste(path,"/Data/Csv/metadataCompras.csv", sep="")
-oferantes <- read_csv(path_oferantes)
+path_oferentes <- paste(path,"/Data/Csv/metadataOferentes.csv", sep = "")
+path_adj <- paste(path,"/Data/Csv/metadataAdjudicaciones.csv", sep = "")
+path_comp <- paste(path,"/Data/Csv/metadataCompras.csv", sep = "")
+oferentes <- read_csv(path_oferentes)
 adjudicaciones <- read_csv(path_adj)
 compras <- read_csv(path_comp)
 
@@ -17,11 +17,9 @@ compList <- c(
   #"apel", "es_reiteracion", 
   "estado_compra", 
   #"fondos_rotatorios",
-  "id_inciso","id_moneda","id_tipo_resol","id_tipocompra","id_ue",
-  "subtipo_compra",
-  "id_ucc"
-  )
-adjList <- c("id_moneda", "id_unidad","tipo_doc_prov")
+  "id_inciso","id_moneda","id_tipo_resol","id_tipocompra","id_ue", "subtipo_compra", "id_ucc"
+)
+adjList <- c("id_moneda", "id_unidad", "tipo_doc_prov")
 
 ofeList <- c("tipo_doc_prov")
 
@@ -29,18 +27,17 @@ ofeList <- c("tipo_doc_prov")
 ui <- tagList(
   shinythemes::themeSelector(),
   navbarPage(
-    "Metadata - Compras Estatales",
-    tabPanel("Navbar 1",
+    "Compras Estatales",
+    tabPanel("Metadata",
              sidebarPanel(
                radioButtons("metadata", "Metadata segun datasets: ",
                             c("Compras Estatales" = "compras" ,
                               "Adjudicaciones" = "adjudicaciones",
-                              "Oferentes" = "oferantes"), 
+                              "Oferentes" = "oferentes"), 
                             selected = 'compras'),
                conditionalPanel(
                  condition = "input.metadata == 'compras'",
-                 selectInput("cod" , "Codigos de Atributos en Compras",
-                             compList)
+                 selectInput("cod" , "Codigos de Atributos en Compras", compList)
                ),
                conditionalPanel(
                  condition = "input.metadata == 'adjudicaciones'",
@@ -48,21 +45,20 @@ ui <- tagList(
                              adjList)
                ),
                conditionalPanel(
-                 condition = "input.metadata == 'oferantes'",
+                 condition = "input.metadata == 'oferentes'",
                  selectInput("cod" , "Codigos de Atributos en Oferentes",
                              ofeList)
                ),
-               actionButton("controller", "Codigueras Existentes",icon("paper-plane"), 
+               actionButton("controller", "Codigueras Existentes", icon("paper-plane"), 
                             style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
-            ),
+             ),
              mainPanel(
                tabsetPanel(id = "inTabset",
-                 tabPanel("Metadata",
-                          # h4("Metadatos"),
-                          tableOutput("table")
-                          
-                 ),
-                 tabPanel(title = "Codigueras", value = "codiguera", tableOutput("codigueTable"))
+                           tabPanel("Metadata",
+                                    # h4("Metadatos"),
+                                    tableOutput("table")
+                           ),
+                           tabPanel(title = "Codigueras", value = "codiguera", tableOutput("codigueTable"))
                )
              )
     )
@@ -85,11 +81,12 @@ server <- function(input, output, session) {
   
   output$codigueTable <- function() {
     
-    codTable <- readRDS(paste(path,"/Data/rds/meta_",input$cod,".rds", sep=""))
-    
+    codTable <- readr::read_rds(paste(path, "/Data/rds/meta_", input$cod, ".rds", sep = ""))
+    names(codTable) <- str_to_title(str_replace_all(string = names(codTable), pattern = "_", replacement = " "))
+
     codTable %>% 
       kableExtra::kable() %>% 
-      kableExtra::kable_styling(bootstrap_options = c("striped", "hover", "responsive"), fixed_thead = TRUE)
+      kableExtra::kable_styling(bootstrap_options = c("striped", "hover", "responsive"), fixed_thead = TRUE, full_width = FALSE)
   }
 }
 
