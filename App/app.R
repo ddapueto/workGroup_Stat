710435a4f3ba03aacd3fa9f8d95b9863e585886b
emamarsella <c.e.marsella@gmail.com>
2019-07-01 14:41:06 -0300

    Cambio algo que podría dar problemas en la shiny, la cual sigue sin funcionar.
    (Hago el commit que realmente quería hacer recién, pero que hice otra cosa)

--- App/app.R
+++ App/app.R
@@ -1,9 +1,8 @@
+
 library(shiny)
 library(shinythemes)
 library(here)
 library(tidyverse)
-library(lubridate)
-library(rlang)
 library(lubridate)
 
 
@@ -81,11 +80,11 @@ ui <- tagList(
                             c("Serie de tiempo de monto total adjudicado por semana" = "monto_adjj" ,
                               "Serie de tiempo de cantidad de adjudicaciones por semana" = "cant_adjj"), 
                             selected = 'monto_adj'
-                ), h4("Filtros a aplicar a las visualizaciones:"),
+               ), h4("Filtros a aplicar a las visualizaciones:"),
                selectInput("filtro_inciso", "Inciso:", incisoList, selected = "No filtrar"),
                selectInput("filtro_moneda", "Moneda:", monedaList, selected = "No filtrar"),
                selectInput("filtro_tipo_compra", "Tipo de Compra:", estcompraList, selected = "No filtrar")
-              ),
+             ),
              mainPanel(
                h3("Serie de tiempo de monto adjudicado por semana."), align="center",
                plotOutput("monto_adj"),
@@ -114,33 +113,49 @@ server <- function(input, output, session) {
     
     codTable <- readr::read_rds(paste(path, "/Data/rds/meta_", input$cod, ".rds", sep = ""))
     names(codTable) <- str_to_title(str_replace_all(string = names(codTable), pattern = "_", replacement = " "))
-
+    
     codTable %>% 
       kableExtra::kable() %>% 
       kableExtra::kable_styling(bootstrap_options = c("striped", "hover", "responsive"), fixed_thead = TRUE, full_width = FALSE)
   }
-
-datos <- base_compras
-
+  
+  datos <- base_compras
+  reactive(if (!!sym(input$filtro_inciso) != "No filtrar") {datos <- datos %>% filter(inciso == !!sym(input$filtro_inciso))}
+  )
+  reactive(if (!!sym(input$filtro_moneda) != "No filtrar") {datos <- datos %>% filter(inciso == !!sym(input$filtro_moneda))}
+  )
+  reactive(if (!!sym(input$filtro_tipo_compra) != "No filtrar") {datos <- datos %>% filter(inciso == !!sym(input$filtro_tipo_compra))}
+  )
+  
   
   output$monto_adj <- renderPlot({
     datos %>% 
-      if (!!sym(input$filtro_inciso) != "No filtrar") {datos %>% filter(inciso == !!sym(input$filtro_inciso))} %>% 
-        if (!!sym(input$filtro_moneda) != "No filtrar") {datos %>% filter(inciso == !!sym(input$filtro_moneda))} %>% 
-          if (!!sym(input$filtro_tipo_compra) != "No filtrar") {datos %>% filter(inciso == !!sym(input$filtro_tipo_compra))}
-
+      filter("2018-01-01" < fecha_compra, fecha_compra < "2018-12-31", monto_adj > 0) %>%
+      group_by(floor_date(fecha_compra, "week")) %>% 
+      summarise(total_comprado = sum(monto_adj_pesos, na.rm = TRUE)) %>% 
+      ggplot() +
+      geom_line(aes(`floor_date(fecha_compra, "week")`, total_comprado), color = "purple") +
+      labs(x = NULL, y = "Monto total adjudicado por semana\n(equivalente en millones de pesos corrientes)\n") +
+      scale_y_continuous(labels = scales::dollar_format(prefix = "$U", big.mark = ".", decimal.mark = ",", scale = 1/1e6)) +
+      ggthemes::theme_economist() +
+      theme(axis.title = element_text(face = "bold"))
   })
   
   output$cant_adj <- renderPlot({
     datos %>% 
-      if (!!sym(input$filtro_inciso) != "No filtrar") {datos %>% filter(inciso == !!sym(input$filtro_inciso))} %>% 
-      if (!!sym(input$filtro_moneda) != "No filtrar") {datos %>% filter(inciso == !!sym(input$filtro_moneda))} %>% 
-      if (!!sym(input$filtro_tipo_compra) != "No filtrar") {datos %>% filter(inciso == !!sym(input$filtro_tipo_compra))}
-      
+      filter("2018-01-01" < fecha_compra, fecha_compra < "2018-12-30", monto_adj > 0) %>%
+      group_by(floor_date(fecha_compra, "week")) %>% 
+      tally() %>% 
+      ggplot() +
+      geom_line(aes(`floor_date(fecha_compra, "week")`, n), color = "orange") +
+      labs(x = NULL, y = "Cantidad de adjudicaciones mensuales\n") +
+      scale_y_continuous(labels = scales::number_format(big.mark = ".", decimal.mark = ",")) +
+      ggthemes::theme_economist() +
+      theme(axis.title = element_text(face = "bold"))
   })
-
+  
 }
 
-                 
+
 # Run the application 
 shinyApp(ui = ui, server = server)
